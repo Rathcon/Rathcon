2bd41a2d4947e6053fc5769f41e116689e7e6b1a
Eshan Iqbal <wahuwalazi@gmail.com>
2025-08-05 03:12:37 +0000

    i have added logo to /public/logo.png use that path please on navbar and

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <Image src="https://i.imgur.com/uV5w229.png" alt="Rathcon Constructions Logo" width={40} height={40} />
+      <Image src="/logo.png" alt="Rathcon Constructions Logo" width={40} height={40} />
       <span className="text-xl font-bold font-headline text-primary">
         Rathcon Constructions
       </span>
